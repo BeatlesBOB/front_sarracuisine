d00e6336219239b21d0eb228c1624c8e13a9d720
BeatlesBOB <38206766+BeatlesBOB@users.noreply.github.com>
2022-09-07 08:57:05 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import ProductSlider from '../components/ProductSlider'
 import Story from '../components/Story'
 
 export default function Home() {
